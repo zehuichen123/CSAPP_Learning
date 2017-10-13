e542b4165e3ec89654c0788af37f9fc4a8c614ab
kirito <kirito@kiritodeMacBook-Pro.local>
2017-10-13 21:15:04 +0800

    Chapter_5 started at 10.13

---
+++ Chapter_5/homework.c
@@ -0,0 +1,17 @@
+5.1
+0
+5.2
+we can do this simple problem with 
+linear programming.Ignored.
+5.3
+Ignored
+5.4
+A.for O2 combine3,it works like more similiar
+to curr in combine4.It is a temporary variable,
+which store the result inside the loop but the 
+difference between it and combine4 is that it 
+refresh *dest every loop to gurantee *dest are
+always the newest value.
+B.Same.
+C.This optimization will not influence the 
+performance of the origin function. 
