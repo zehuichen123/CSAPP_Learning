5ca6cdc74328fa4364e8b0abb030645815c0a601
kirito <kirito@kiritodeMacBook-Pro.local>
2017-10-17 16:45:49 +0800

    until 5.9

--- Chapter_5/homework.c
+++ Chapter_5/homework.c
@@ -15,3 +15,37 @@ always the newest value.
 B.Same.
 C.This optimization will not influence the 
 performance of the origin function. 
+5.5
+A.2n multification and n addition
+B.To get the CPE of this circulation, we need 
+to make the key path. In this case, we just 
+need to calculate xpwr before we start the next
+circulation. So, we just need to think out x=x*
+xpwr; And obviously, the CPE of this statement 
+is 5/1(only one multification)
+5.6
+A.n multification and n addition
+B.As we mentioned before, the key path of this 
+process is result, in other words, we must calculate
+ variable result out before we start another ci-
+ rculation. So ,the CPE of this statem is 3(addition)
+ +5(multification).
+C.A stupid question. We have already known that
+the CPE of the 1st case is 5 and the 2nd case is 8.
+Why I need to explain more???
+5.7
+just change one for statement in figure5-16: 
+for(i=0;i<length-4;i+=5){
+	acc=((((acc OP data[i]) OP data[i+1])
+		OP data[i+2]) OP data[i+3]) OP data[i+4];
+}
+5.8
+For in each multification, we would calculate 3
+elements all together at a time. So do not forget
+to divide 3 when calculate CPE:
+A1:we always need to know r before we calculate,
+so it is 15/3=5
+A2:10/3=3.33
+A3:5/3=1.67
+A4:5/3=1.67
+A5:10/3=3.33
