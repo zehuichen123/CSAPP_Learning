4049d941ac9bb31495bbb66d77f215539c40028b
loveSnowBest <lovesnowbest@gmail.com>
2017-11-08 17:16:34 +0800

    CMU lab for csapp and my answer

---
+++ csapp_lab/03_attack_handout/temp.d
@@ -0,0 +1,10 @@
+
+temp.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 c7 c7 78 dc 61 55 	mov    $0x5561dc78,%rdi
+   7:	48 83 c4 10          	add    $0x10,%rsp
+   b:	c3                   	retq   
